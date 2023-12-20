ce6c550e743d87c4448ebf2e83635437e99505f3
dicenull <dicenull.dev@gmail.com>
2023-12-20 18:51:06 +0900

    modal design

--- lib/pages/detail_modal.dart
+++ lib/pages/detail_modal.dart
@@ -51,6 +51,7 @@ class OrderMenuDetailModal extends HookConsumerWidget {
     };
     return DraggableScrollableSheet(
       initialChildSize: 1.0,
+      snap: true,
       builder: (BuildContext context, ScrollController scrollController) {
         return Container(
           child: res.when(
@@ -58,86 +59,110 @@ class OrderMenuDetailModal extends HookConsumerWidget {
               return Stack(
                 children: [
                   Positioned.fill(
-                    child: ListView(
-                      controller: scrollController,
-                      padding: const EdgeInsets.symmetric(horizontal: 28),
+                    child: Column(
                       children: [
                         Container(
                           padding: const EdgeInsets.only(top: 48),
-                          child: Container(
-                            decoration: const BoxDecoration(
-                              image: DecorationImage(
-                                fit: BoxFit.cover,
-                                image: AssetImage('assets/background.jpg'),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Icon(
+                                Icons.keyboard_arrow_left,
+                                size: 40,
+                                color: Colors.white.withOpacity(.8),
                               ),
-                            ),
-                            width: 200,
-                            height: 200,
+                              Container(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 28),
+                                child: const Image(
+                                  width: 200,
+                                  height: 200,
+                                  image: AssetImage('assets/background.jpg'),
+                                ),
+                              ),
+                              Icon(
+                                Icons.keyboard_arrow_right,
+                                size: 40,
+                                color: Colors.white.withOpacity(.8),
+                              ),
+                            ],
                           ),
                         ),
                         Container(
                           padding: const EdgeInsets.only(top: 23),
                           child: Text(
                             orderMenu.name,
+                            textAlign: TextAlign.center,
                             style: const TextStyle(
-                                fontSize: 24,
-                                fontWeight: FontWeight.w700,
-                                color: Colors.white),
-                          ),
-                        ),
-                        Container(
-                          padding: const EdgeInsets.only(top: 24),
-                          child: Text(
-                            "● Alc. ${orderMenu.alcPercent}%\n● ジン、トニックウォーター、ライム",
-                            style: const TextStyle(
-                                fontSize: 14,
-                                fontWeight: FontWeight.w400,
-                                color: Colors.white),
-                          ),
-                        ),
-                        Container(
-                          padding: const EdgeInsets.only(top: 24),
-                          child: SizedBox(
-                            width: 247,
-                            child: AutoSizeText(
-                              orderMenu.description,
-                              style: const TextStyle(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w400,
-                                  color: Colors.white),
+                              fontSize: 24,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.white,
                             ),
                           ),
                         ),
-                        const SizedBox(height: 80),
+                        Flexible(
+                          child: ListView(
+                            children: [
+                              Container(
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: 24,
+                                  horizontal: 64,
+                                ),
+                                child: Text(
+                                  "● Alc. ${orderMenu.alcPercent}%\n● ジン、トニックウォーター、ライム",
+                                  style: const TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w400,
+                                      color: Colors.white),
+                                ),
+                              ),
+                              Container(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 64),
+                                child: SizedBox(
+                                  width: 247,
+                                  child: AutoSizeText(
+                                    orderMenu.description,
+                                    style: const TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w400,
+                                        color: Colors.white),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(height: 150),
+                            ],
+                          ),
+                        ),
                       ],
                     ),
                   ),
                   Align(
                     alignment: Alignment.bottomCenter,
                     child: Padding(
-                      padding: const EdgeInsets.all(16),
+                      padding: const EdgeInsets.symmetric(vertical: 48),
                       child: SizedBox(
                         width: 247,
                         height: 48,
                         child: FilledButton(
-                            onPressed: ref.watch(orderButtonProvider) ==
-                                    OrderButtonType.before
-                                ? () async {
-                                    ref
-                                        .read(orderButtonProvider.notifier)
-                                        .state = OrderButtonType.processing;
-                                    final res = await ref
-                                        .read(cocktailOrderProvider(id).future);
-                                    ref.read(orderNumProvider.notifier).state =
-                                        res;
-                                    ref
-                                        .read(orderButtonProvider.notifier)
-                                        .state = OrderButtonType.done;
-                                  }
-                                : null,
-                            style: FilledButton.styleFrom(
-                                backgroundColor: Colors.white),
-                            child: buttonText),
+                          onPressed: ref.watch(orderButtonProvider) ==
+                                  OrderButtonType.before
+                              ? () async {
+                                  ref.read(orderButtonProvider.notifier).state =
+                                      OrderButtonType.processing;
+                                  final res = await ref
+                                      .read(cocktailOrderProvider(id).future);
+                                  ref.read(orderNumProvider.notifier).state =
+                                      res;
+                                  ref.read(orderButtonProvider.notifier).state =
+                                      OrderButtonType.done;
+                                }
+                              : null,
+                          style: FilledButton.styleFrom(
+                              backgroundColor: Colors.white),
+                          child: buttonText,
+                        ),
                       ),
                     ),
                   ),
