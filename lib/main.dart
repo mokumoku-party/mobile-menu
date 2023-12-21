9d7ec4c79454c2c0f3925b7eb6f0ed4c314c9d4d
dicenull <dicenull.dev@gmail.com>
2023-12-21 09:21:13 +0900

    no sharp url path

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:app/app.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-
+  if (kIsWeb) {
+    setUrlStrategy(PathUrlStrategy());
+  }
   runApp(const ProviderScope(child: App()));
 }
