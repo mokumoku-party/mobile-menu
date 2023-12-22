99187da9d166aaf59e214b41d3bab885482fb9d6
dicenull <dicenull.dev@gmail.com>
2023-12-22 12:46:37 +0900

    assets only web

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,6 +8,7 @@ import 'package:app/models/order_menu_state.dart';
 import 'package:app/models/order_provider.dart';
 import 'package:app/pages/detail_modal.dart';
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -91,7 +92,8 @@ class HomePage extends HookConsumerWidget {
               color: Colors.transparent,
               image: DecorationImage(
                 fit: BoxFit.cover,
-                image: AssetImage('assets/background.png'),
+                image: AssetImage(
+                    '${kIsWeb ? 'assets/' : ''}assets//background.png'),
               ),
             ),
           ),
